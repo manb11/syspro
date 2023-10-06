1fcbb83521231282d43d873cdee61e9290acbb20
manb11 <tjdrhd126@naver.com>
2023-10-06 05:52:40 +0000

    10/6

---
+++ chap3/prob2/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "copy.h" 
+#include <string.h>
+
+char line[MAXLINE] ;
+char longest[MAXLINE] ;
+
+int main()
+{	
+	int len=0 ;
+	int max=0 ;
+
+	while (fgets(line,MAXLINE,stdin) != NULL) 
+	{
+		len = strlen(line);
+
+		if (len > max) {
+			max = len;
+			copy(line, longest);
+		}
+	}	
+	if(max > 0)
+		printf("%s \n", longest);
+
+
+	return 0;
+}
