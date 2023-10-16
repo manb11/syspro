a54f16df3d9594297829ddc72c630dbf19ddbec0
manb11 <tjdrhd126@naver.com>
2023-10-16 23:31:39 +0900

    10/16

--- chap3/prob2/main.c
+++ chap3/prob2/main.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
-#include "copy.h" 
 #include <string.h>
+#include "copy.h"
 
-char line[5] ;
-char longest[MAXLINE] ;
+int main() {
+    char line[5][MAXLINE];
+    char temp[MAXLINE]; 
 
-int main()
-{	
-	int len=0 ;
-	int max=0 ;
+    int i = 0;
+    while (i < 5) {
+        if (fgets(line[i], sizeof(line[i]), stdin) == NULL) {
+            return 1;
+        }
+        size_t len = strlen(line[i]);
+        if (len > 0 && line[i][len - 1] == '\n') {
+            line[i][len - 1] = '\0';
+        }
+        i++;
+    }
 
+    for (i = 0; i < 5; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            if (strlen(line[i]) < strlen(line[j])) {
+                strcpy(temp, line[i]);
+                strcpy(line[i], line[j]);
+                strcpy(line[j], temp);
+            }
+        }
+    }
 
-	while (fgets(line,MAXLINE,stdin) != NULL) 
-	{
-		len = strlen(line);
+    for (i = 0; i < 5; i++) {
+        printf("%s\n", line[i]);
+    }
 
-		if (len > max) {
-			max = len;
-			copy(line, longest);
-		}
-	}	
-	if(max > 0)
-	
-		printf("%s \n", longest);
-	
-
-	return 0;
+    return 0;
 }
+
