d9c11223f2062b20295f673a3edc453edf314a8b
manb11 <tjdrhd126@naver.com>
2023-10-25 13:55:56 +0900

    10/25

--- chap4/prob10/cat.c
+++ chap4/prob10/cat.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
-{
-int main(int argc, char *argv[])
-   FILE *fp;
-   int c;
-   if (argc < 2) 
-      fp = stdin; 
-   else  fp = fopen(argv[1],"r");
+#include <stdlib.h>
+#include <string.h>
 
-   c = getc(fp); 
-   while (c != EOF) { 	
-      putc(c, stdout); 
-      c = getc(fp); 
-   }
-   fclose(fp); 
-   return 0;
+int main(int argc, char *argv[]) {
+    int line_number = 1;
+    int print_line_numbers = 0;
+
+    if (argc < 2) {
+        printf("Usage: %s [-n] file1 file2 ...\n", argv[0]);
+        return 1;
+    }
+
+    int arg_index = 1;
+
+    if (argc > 2 && strcmp(argv[1], "-n") == 0) {
+        print_line_numbers = 1;
+        arg_index = 2;
+    }
+
+    for (; arg_index < argc; arg_index++) {
+        FILE *fp = fopen(argv[arg_index], "r");
+        if (fp == NULL) {
+            fprintf(stderr, "Error opening file: %s\n", argv[arg_index]);
+            continue;
+        }
+
+        char line[1024];
+        while (fgets(line, sizeof(line), fp) != NULL) {
+            if (print_line_numbers) {
+                printf("%d ", line_number);
+            }
+            printf("%s", line);
+            line_number++;
+        }
+
+        fclose(fp);
+    }
+
+    return 0;
 }
 
