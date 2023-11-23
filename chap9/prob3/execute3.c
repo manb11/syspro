e6063eda9922e57aa87ad6a6381020ead95b07d4
manb11 <tjdrhd126@naver.com>
2023-11-23 08:45:48 +0000

    11/23

---
+++ chap9/prob3/execute3.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{ 
+   int child, pid, status;
+   pid = fork( );
+   if (pid == 0) { // 자식 프로세스
+      execvp(argv[1], &argv[1]);
+      fprintf(stderr, "unexecute \n",argv[1]);
+   } else { // 부모 프로세스
+      child = wait(&status);
+      printf("[%d] child process %d end \n", getpid(), pid);
+      printf("\t end code %d \n", status>>8);
+   }
+}
+
+
